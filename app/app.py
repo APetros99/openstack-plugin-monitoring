0455413f6b57dcb3783944a364fcc8b8c06990f2
APetros99 <alfonso.petrosino99@libero.it>
2025-03-15 15:39:58 +0100

    fix

--- app/app.py
+++ app/app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
+import openstack
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    print("La route / è stata chiamata!")
-    return 'Hello, World!'
+# Connessione a OpenStack
+conn = openstack.connect()
+
+@app.route('/status')
+def get_openstack_status():
+    # Recupera tutte le istanze attive
+    servers = conn.compute.servers()
+    instances = [{"name": s.name, "status": s.status} for s in servers]
+
+    # Recupera i volumi
+    volumes = conn.block_storage.volumes()
+    volume_list = [{"name": v.name, "size": v.size, "status": v.status} for v in volumes]
+
+    # Recupera le reti
+    networks = conn.network.networks()
+    network_list = [{"name": n.name, "status": n.status} for n in networks]
+
+    return jsonify({
+        "instances": instances,
+        "volumes": volume_list,
+        "networks": network_list
+    })
 
 if __name__ == '__main__':
-    print("Avvio dell'app Flask...")
-    app.run(host='0.0.0.0', port=5234)
+    app.run(host='0.0.0.0', port=5000)
