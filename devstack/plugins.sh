031b984f41d0e94ed2feced5def9ada1b379eab4
APetros99 <alfonso.petrosino99@libero.it>
2025-03-15 11:00:44 +0100

    modified plugin.sh

--- devstack/plugins.sh
+++ devstack/plugins.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Carica il file settings
+# Carica settings
 source /opt/stack/openstack-plugin-monitoring/devstack/settings
 
-# Installazione di Grafana
 function install_grafana {
     echo "Installing Grafana..."
+    sudo apt update
+    sudo apt install -y apt-transport-https software-properties-common wget
+    wget -q -O - https://apt.grafana.com/gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/grafana.gpg
+    echo "deb https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
     sudo apt update
     sudo apt install -y grafana
     sudo systemctl enable grafana-server
@@ -13,7 +16,11 @@ function install_grafana {
     echo "Grafana installed successfully!"
 }
 
-# Funzione per disinstallare e pulire
+function configure_grafana {
+    echo "Configuring Grafana..."
+    # (opzionale) inserire qui eventuali configurazioni aggiuntive, ad esempio datasource OpenStack
+}
+
 function uninstall_grafana {
     echo "Removing Grafana..."
     sudo systemctl stop grafana-server
@@ -23,19 +30,19 @@ function uninstall_grafana {
 }
 
 if is_service_enabled openstack-plugin-monitoring; then
-    case "$1" in
-        stack)
-            case "$2" in
-                install)
-                    install_grafana
-                    ;;
-                post-config)
-                    # eventuale configurazione aggiuntiva qui
-                    ;;
-            esac
-            ;;
-        unstack)
-            uninstall_grafana
-            ;;
-    esac
+    if [[ "$1" == "stack" && "$2" == "install" ]]; then
+        install_grafana
+    fi
+
+    if [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        echo "No additional post-config steps required."
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        uninstall_grafana
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        uninstall_grafana
+    fi
 fi
